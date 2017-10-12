42334a057b85d7b3daad42e11f7f94b2ecdbc997
AHowardC <achsvgusaeng@yahoo.com>
2017-10-12 12:11:42 -0400

    some css

--- scripts.js
+++ scripts.js
@@ -4,6 +4,47 @@
 // 4. Get the response from Yahoo and update teh DOM
 
 $(document).ready(()=>{
+
+	// setItem takes 2 args:
+	// 1. Name of the var
+	// // 2. Value to set
+	// var watchList = [
+	// 	"goog",
+	// 	"msft",
+	// 	"tsla",
+	// 	"tata",
+	// 	"race"
+	// ];
+	// ENTER... JSON.stringify
+	// var watchListAsString = JSON.stringify(watchList);
+
+	// console.log(typeof(watchList));
+	// console.log(typeof(watchListAsString));
+
+	// console.log(watchList)
+	// console.log(watchListAsString)
+
+	// // ENTER... JSON.parse
+	// var watchListAsAnObjectAgain = JSON.parse(watchListAsString);
+	// console.log(watchListAsAnObjectAgain);
+
+
+	// localStorage.setItem('watchList',"race");
+	// var watchList = localStorage.getItem('watchList');
+	// console.log(localStorage.watchList);
+
+
+	// var now = new Date();
+
+	// Get the watchlist
+	var watchList = localStorage.getItem('watchList');
+	// If it's null, they dont ahve any saved.
+	if(watchList !== null){
+		// if it's not, update the DOM
+		updateWatchList();		
+	}
+
+
 	var firstView = true;
 	$('.yahoo-finance-form').submit((event)=>{
 		// Stop the browser from sending the page on... we will handle it.
@@ -35,17 +76,58 @@ $(document).ready(()=>{
 			}else{
 				$('#stock-table-body').append(newRow);
 			}
+
+			// Rocket function vs. function():
+			// - rocket function does NOT create a new "this"
+			// - function() "usually" creates a new "this"
+			$('.save-button').click(function(){
+				// console.log("User clicked on a button!")
+				// console.log($(this).attr('symbol'));
+				// add a click listener to all the buttons in the tables.
+				// when clicked on, save the symbol to localStorage.
+				var stockToSave = $(this).attr('symbol');
+				var oldWatchlist = localStorage.getItem('watchList');
+				// oldWatchlist just came out of localStorage. Like Christmas lights come otu of storage.
+				// How? TANGLED. We need to untangle them. JSON.parse
+				var oldAsJSON = JSON.parse(oldWatchlist);
+				// if the user has never saved anything, there will be nothing
+				// 	to parse. This will reutnr null in JSON.parse
+				if(oldAsJSON === null){
+					oldAsJSON = [];
+				}
+				// JSON.parse has just untangled our lights. We have an object/array
+
+				// Before we push it on to the array, Check to see if its in the array...
+				// indexOf to the rescue.
+				if(oldAsJSON.indexOf(stockToSave) > -1){
+					// the stock is already in the list. 
+					// We don't know where, but it's there because it didn't
+					// return -1
+					console.log("Stock already saved.")
+				}else{
+					oldAsJSON.push(stockToSave);
+					console.log(oldAsJSON);
+					var newWatchListAsString = JSON.stringify(oldAsJSON);
+					localStorage.setItem('watchList',newWatchListAsString);
+					updateWatchList()
+				}
+			})
 		});
 	})
 
 	function buildRow(stockInfo){
-		if(stockInfo.Change.indexOf('+') > -1){
-			// That means the stock is up!
-			var classChange = "success";
+		if(stockInfo.Change !== null){
+			if(stockInfo.Change.indexOf('+') > -1){
+				// That means the stock is up!
+				var classChange = "success";
+			}else{
+				// Stock is down :(
+				var classChange = "danger";
+			}			
 		}else{
-			// Stock is down :(
-			var classChange = "danger";
+			stockInfo.Change = "Market Closed."
 		}
+
 		var newRow = '';
 		newRow += '<tr>'; 
 			newRow += `<td>${stockInfo.Symbol}</td>`;
@@ -53,7 +135,27 @@ $(document).ready(()=>{
 			newRow += `<td>${stockInfo.Ask}</td>`;
 			newRow += `<td>${stockInfo.Bid}</td>`;
 			newRow += `<td class="bg-${classChange}">${stockInfo.Change}</td>`;
+			newRow += `<td><button symbol=${stockInfo.Symbol} class="btn btn-success save-button">Save</button></td>`;
+			newRow += `<td><button symbol=${stockInfo.Symbol} class="btn btn-danger delete-button">Delete</button></td>`;
 		newRow += '</tr>';		
 		return newRow;
 	}
+
+	function updateWatchList(){
+		$('#stock-table-saved-body').html('')
+		// get the watchlist
+		var watchList = localStorage.getItem('watchList');
+		// Problem. The CHristmas Ligths are tangled.
+		var watchListAsJSON = JSON.parse(watchList);
+		watchListAsJSON.map((symbol,index)=>{
+			// console.log(symbol);
+			var url = 'http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22'+symbol+'%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+			$.getJSON(url, (stockData)=>{
+				var stockInfo = stockData.query.results.quote;
+				var newRow = buildRow(stockInfo);
+				$('#stock-table-saved-body').append(newRow);
+			})
+		});
+	}
+
 });
